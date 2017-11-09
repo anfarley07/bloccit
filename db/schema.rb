2bae2fe3785dd94d5e29f6c277036cf88ec99cb8
Amy Farley <amyfarleyseattle@gmail.com>
2017-11-08 22:17:26 -0800

    Issue with answer_spec re body attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108221656) do
+ActiveRecord::Schema.define(version: 20171108223322) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "title"
+    t.text "boday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -24,6 +31,14 @@ ActiveRecord::Schema.define(version: 20171108221656) do
     t.string "title"
     t.string "string"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
