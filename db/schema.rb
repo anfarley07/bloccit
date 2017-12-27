9c1ea1552f53ab7bf685e10e230ec592b22a42f2
Amy Farley <amyfarleyseattle@gmail.com>
2017-12-27 13:37:19 -0500

    DB and model messed up due to dupes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211223951) do
+ActiveRecord::Schema.define(version: 20171227180831) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
