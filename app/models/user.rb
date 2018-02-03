ba8e88826ccc9a2dc04f20151bf6825ff2a357cb
Amy Farley <amyfarleyseattle@gmail.com>
2018-02-03 12:25:02 -0800

    user name validation working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save {
+    newArray = []
+    name.downcase.split().each{|x| newArray.push(x.capitalize)}
+    self.name = newArray.join(" ") }
+
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
