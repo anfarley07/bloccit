2e215d55708b64473e4687546f7eaf1e19a5b9bf
Amy Farley <amyfarleyseattle@gmail.com>
2018-02-04 19:14:21 -0800

    New User form and validation working as expected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
@@ -16,7 +15,7 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
     end
   end
-  
 end
