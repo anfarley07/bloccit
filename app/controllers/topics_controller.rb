e08dc595ab286e67bb43a04bfa34ecf0c584ec01
Amy Farley <amyfarleyseattle@gmail.com>
2018-03-05 17:28:57 -0800

    trying to figure out moderator authentication

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except:[:index, :show]
+  before_action :authorize_user
+
 
   def index
     @topics = Topic.all
@@ -61,7 +62,11 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-     unless current_user.admin?
+     if current_user.admin?
+       can :new, :create, :edit, :update, :destroy
+     elsif current_user.moderator?
+       can :edit, :update
+     else
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
      end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,11 +35,20 @@ RSpec.describe User, type: :model do
     end
 
     it "responds to member?" do
-      expect(user.role).to eql("member")
+      expect(user).to respond_to(:member?)
+    end
+
+    it "responds to moderator?" do
+      expect(user).to respond_to(:moderator?)
     end
   end
 
   describe "roles" do
+
+    it "is member by default" do
+      expect(user.role).to eql("member")
+    end
+
     context "member user" do
       it "returns true for #member" do
         expect(user.member?).to be_truthy
@@ -59,8 +68,30 @@ RSpec.describe User, type: :model do
         expect(user.member?).to be_falsey
       end
 
+      it "returns false for #moderator" do
+        expect(user.moderator?).to be_falsey
+      end
+
       it "returns true for #admin?" do
         expect(user.admin?).to be_truthy
+      end
+    end
+
+    context "moderator user" do
+      before do
+        user.moderator!
+      end
+
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+
+      it "returns true for #moderator?" do
+        expect(user.moderator?).to be_truthy
       end
     end
   end
